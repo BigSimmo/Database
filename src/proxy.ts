ad6806c7ac4b874a281a27e64cec489d707c182d
BigSimmo <87357024+BigSimmo@users.noreply.github.com>
2026-08-05 18:45:09 +0800

    fix(proxy): remove formatter diagnostic from source

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,3 @@
-npm warn Unknown env config "http-proxy". This will stop working in the next major version of npm.
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
